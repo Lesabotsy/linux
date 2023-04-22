40098d462199e5a1dab43e8a489aeaa6f3940ccb
Lesabotsy <moshiro17@gmail.com>
2023-04-23 01:26:28 +0200

    Add files via upload

---
+++ gnome.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# RPM Fusion
+dnf install -y \
+	https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+	https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# Main
+dnf --setopt=install_weak_deps=False --best install -y \
+	@base-x \
+	@"Common NetworkManager Submodules" \
+	@Fonts \
+	@"Hardware Support" \
+	@Multimedia \
+	gnome-shell \
+	power-profiles-daemon
+
+# Applications
+dnf --setopt=install_weak_deps=False --best install -y \
+	aria2 \
+	bat \
+	breeze-cursor-theme \
+	btop \
+	curl \
+	eog \
+	exa \
+	ffmpeg \
+	fd-find \
+	fish \
+	fzf \
+	gnome-tweaks \
+	gnome-extensions-app \
+	gnome-browser-connector \
+	kitty \
+	mupdf \
+	nautilus \
+	neovim \
+	openssl \
+	ripgrep \
+	transmission \
+	unrar \
+	vlc \
+	yt-dlp \
+	yt-dlp-fish-completion
+
+# Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+dnf install -y google-chrome-stable_current_x86_64.rpm
+
+# Programming Languages
+dnf --setopt=install_weak_deps=False --best install -y \
+	python3-pip \
+	java-17-openjdk-devel \
+	gcc gcc-c++ gdb valgrind \
+	nodejs npm
+
+# Starship
+curl -sS https://starship.rs/install.sh | sh
+
+# Removing some Packages
+dnf remove -y nano vim-minimal
+
+# Reboot
+systemctl set-default graphical.target
+reboot
