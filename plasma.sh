600141867d126aed31289bd7c3a628c6bb1fb229
Lesabotsy <moshiro17@gmail.com>
2023-04-23 11:14:55 +0200

    Update plasma.sh

--- plasma.sh
+++ plasma.sh
@@ -17,107 +17,107 @@ dnf --setopt=install_weak_deps=False --best install -y \
 
 # Plasma
 dnf --setopt=install_weak_deps=False --best install -y \
-   plasma-desktop
-   plasma-workspace
-   plasma-workspace-wallpapers
-   qt5-qtbase-gui
-   sddm
-   sddm-breeze
-   sddm-kcm
-   NetworkManager-config-connectivity-fedora
-   PackageKit-command-not-found
-   abrt-desktop
-   adwaita-gtk2-theme
-   akregator
-   bluedevil
-   breeze-icon-theme
-   colord-kde
-   cups-pk-helper
-   dnfdragora
-   dolphin
-   ffmpegthumbs
-   firewall-config
-   flatpak-kcm
-   fprintd-pam
-   glibc-all-langpacks
-   gnome-keyring-pam
-   gwenview
-   initial-setup-gui
-   kaddressbook
-   kamera
-   kcalc
-   kcharselect
-   kde-connect
-   kde-gtk-config
-   kde-partitionmanager
-   kde-print-manager
-   kde-settings-pulseaudio
-   kdegraphics-thumbnailers
-   kdeplasma-addons
-   kdialog
-   kdnssd
-   keditbookmarks
-   kf5-akonadi-server
-   kf5-akonadi-server-mysql
-   kf5-baloo-file
-   kf5-kipi-plugins
-   kfind
-   kgpg
-   khelpcenter
-   khotkeys
-   kinfocenter
-   kio-admin
-   kio-gdrive
-   kmag
-   kmail
-   kmenuedit
-   kmousetool
-   kmouth
-   konsole5
-   kontact
-   korganizer
-   kscreen
-   kscreenlocker
-   ksshaskpass
-   kwalletmanager5
-   kwebkitpart
-   kwin
-   kwrite
-   libappindicator-gtk3
-   okular
-   pam-kwallet
-   phonon-qt5-backend-gstreamer
-   pinentry-qt
-   plasma-breeze
-   plasma-desktop-doc
-   plasma-discover
-   plasma-discover-notifier
-   plasma-disks
-   plasma-drkonqi
-   plasma-nm
-   plasma-nm-l2tp
-   plasma-nm-openconnect
-   plasma-nm-openswan
-   plasma-nm-openvpn
-   plasma-nm-pptp
-   plasma-nm-vpnc
-   plasma-pa
-   plasma-systemmonitor
-   plasma-thunderbolt
-   plasma-vault
-   plasma-welcome
-   plasma-workspace-geolocation
-   plasma-workspace-x11
-   polkit-kde
-   qt5-qtdeclarative
-   spectacle
-   systemd-oomd-defaults
-   udisks2
+   plasma-desktop \
+   plasma-workspace \
+   plasma-workspace-wallpapers \
+   qt5-qtbase-gui \
+   sddm \
+   sddm-breeze \
+   sddm-kcm \
+   NetworkManager-config-connectivity-fedora \
+   PackageKit-command-not-found \
+   abrt-desktop \
+   adwaita-gtk2-theme \
+   akregator \
+   bluedevil \
+   breeze-icon-theme \
+   colord-kde \
+   cups-pk-helper \
+   dnfdragora \
+   dolphin \
+   ffmpegthumbs \
+   firewall-config \
+   flatpak-kcm \
+   fprintd-pam \
+   glibc-all-langpacks \
+   gnome-keyring-pam \
+   gwenview \
+   initial-setup-gui \
+   kaddressbook \
+   kamera \
+   kcalc \
+   kcharselect \
+   kde-connect \
+   kde-gtk-config \
+   kde-partitionmanager \
+   kde-print-manager \
+   kde-settings-pulseaudio \
+   kdegraphics-thumbnailers \
+   kdeplasma-addons \
+   kdialog \
+   kdnssd \
+   keditbookmarks \
+   kf5-akonadi-server \
+   kf5-akonadi-server-mysql \
+   kf5-baloo-file \
+   kf5-kipi-plugins \
+   kfind \
+   kgpg \
+   khelpcenter \
+   khotkeys \
+   kinfocenter \
+   kio-admin \
+   kio-gdrive \
+   kmag \
+   kmail \
+   kmenuedit \
+   kmousetool \
+   kmouth \
+   konsole5 \
+   kontact \
+   korganizer \
+   kscreen \
+   kscreenlocker \
+   ksshaskpass \
+   kwalletmanager5 \
+   kwebkitpart \
+   kwin \
+   kwrite \
+   libappindicator-gtk3 \
+   okular \
+   pam-kwallet \
+   phonon-qt5-backend-gstreamer \
+   pinentry-qt \
+   plasma-breeze \
+   plasma-desktop-doc \
+   plasma-discover \
+   plasma-discover-notifier \
+   plasma-disks \
+   plasma-drkonqi \
+   plasma-nm \
+   plasma-nm-l2tp \
+   plasma-nm-openconnect \
+   plasma-nm-openswan \
+   plasma-nm-openvpn \
+   plasma-nm-pptp \
+   plasma-nm-vpnc \
+   plasma-pa \
+   plasma-systemmonitor \
+   plasma-thunderbolt \
+   plasma-vault \
+   plasma-welcome \
+   plasma-workspace-geolocation \
+   plasma-workspace-x11 \
+   polkit-kde \
+   qt5-qtdeclarative \
+   spectacle \
+   systemd-oomd-defaults \
+   udisks2 \
    xorg-x11-drv-libinput
 
 # Applications
 dnf --setopt=install_weak_deps=False --best install -y \
-aria2 \
+	aria2 \
 	bat \
 	breeze-cursor-theme \
 	btop \
