7a7219ec8174d2b73997d9db44060297612174b1
Lesabotsy <moshiro17@gmail.com>
2023-06-09 09:10:25 +0200

    Update sway.sh

--- sway.sh
+++ sway.sh
@@ -53,6 +53,11 @@ dnf --setopt=install_weak_deps=False --best install -y \
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
 dnf install -y google-chrome-stable_current_x86_64.rpm
 
+# VSCode
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf --setopt=install_weak_deps=False --best install -y code
+
 # SETUP
 dnf --setopt=install_weak_deps=False --best install -y \
 	python3-pip \
