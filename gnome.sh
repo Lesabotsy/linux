ad9174a7d15ced540975014449332316588a68ae
Lesabotsy <moshiro17@gmail.com>
2023-07-22 10:23:48 +0200

    Add files via upload

---
+++ gnome.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# RPM Fusion
+dnf install -y \
+	https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+	https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# Main
+dnf --setopt=install_weak_deps=False --best install -y \
+	@"base-x" \
+	@"Common NetworkManager Submodules" \
+	@"Fonts" \
+	@"Hardware Support" \
+	@"Multimedia" \
+	gnome-shell \
+	power-profiles-daemon
+
+# Applications
+dnf --setopt=install_weak_deps=False --best install -y \
+	aria2 \
+	bat \
+	breeze-cursor-theme \
+	btop \
+	curl \
+	eog \
+	exa \
+	fd-find \
+	fish \
+	fzf \
+	gnome-browser-connector gnome-extensions-app gnome-tweaks \
+	kitty \
+	mpv \
+	nautilus gvfs-mtp \
+	neovim \
+	openssl \
+	ripgrep \
+	transmission \
+	unrar \
+	yt-dlp yt-dlp-fish-completion
+
+# Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+dnf install -y google-chrome-stable_current_x86_64.rpm
+
+# VSCode
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf install -y code
+
+# Programming Languages
+dnf --setopt=install_weak_deps=False --best install -y \
+	python3-pip python3-autopep8 \
+	gcc gcc-c++ gdb valgrind \
+	nodejs npm
+
+# Starship
+curl -sS https://starship.rs/install.sh | sh
+
+# Removing some Packages
+dnf remove -y nano vim-minimal
+
+# Reboot
+systemctl set-default graphical.target
+reboot
---
+++ kickstart.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+apt purge --autoremove -y \
+	kate kwrite \
+	konqueror \
+	nano vim-common vim-tiny
+
+# UPDATES + APPS
+apt update && apt upgrade -y
+apt install --no-install-recommends -y \
+	aria2 \
+	bat \
+	curl \
+	exa \
+	fd-find \
+	fish \
+	ffmpeg \
+	fzf \
+	gwenview \
+	kde-spectacle \
+	mpv \
+	neovim \
+	okular okular-extra-backends \
+	transmission \
+	unzip unrar-free \
+	yt-dlp \
+	zip
+
+# PROGRAMMING LANGUAGES
+apt install --no-install-recommends -y \
+	gcc gdb g++ valgrind \
+	openjdk-17-jdk \
+	python3-pip python3-autopep8
+
+curl -fsSL https://deb.nodesource.com/setup_18.x | bash - &&
+	apt-get install -y nodejs
+
+# CHROME
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg --install google-chrome-stable_current_amd64.deb
+apt install -f
+rm google-chrome-stable_current_amd64.deb
+
+# VSCODE
+wget https://az764295.vo.msecnd.net/stable/695af097c7bd098fbf017ce3ac85e09bbc5dda06/code_1.79.2-1686734195_amd64.deb
+dpkg --install code_1.79.2-1686734195_amd64.deb
+rm code_1.79.2-1686734195_amd64.deb
+
+# STARSHIP
+curl -sS https://starship.rs/install.sh | sh
+
+# FONTS
+cp -r /home/ralinoro/.fonts/fontsTT/* /usr/share/fonts/truetype/
+cp -r /home/ralinoro/.fonts/fontsOT/* /usr/share/fonts/opentype/
+rm -r /home/ralinoro/.fonts/
