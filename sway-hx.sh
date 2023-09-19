5a687532b6ba64603e9d9f6c25e08097909b6f1c
Lesabotsy <moshiro17@gmail.com>
2023-09-19 11:29:19 +0200

    Update sway-hx.sh

--- sway-hx.sh
+++ sway-hx.sh
@@ -57,7 +57,6 @@ dnf install -y google-chrome-stable_current_x86_64.rpm
 # LANGUAGES & TOOLS
 dnf --setopt=install_weak_deps=False --best install -y \
 	python3-pip black \
-	java-17-openjdk-devel \
 	gcc gcc-c++ gdb valgrind clang-tools-extra \
 	nodejs npm
 
