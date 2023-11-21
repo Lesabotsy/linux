41cacec33b5681e1ac421e144d86c3e59a32a145
Lesabotsy <moshiro17@gmail.com>
2023-11-21 23:54:22 +0100

    Update fedora-sway.sh

--- fedora-sway.sh
+++ fedora-sway.sh
@@ -59,6 +59,24 @@ dnf --setopt=install_weak_deps=False --best install -y \
     arc-theme breeze-cursor-theme papirus-icon-theme \
     lxappearance
 
+# LANGUAGES & TOOLS
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf check-update
+dnf --setopt=install_weak_deps=False --best install -y code
+
+dnf --setopt=install_weak_deps=False --best install -y \
+    gcc gcc-c++ gdb valgrind clang-tools-extra \
+    nodejs npm \
+    python3-pip black \
+    rust cargo rust-analyzer \
+    stack
+
+npm install -g \
+    pyright \
+    typescript typescript-language-server \
+    vscode-langservers-extracted
+
 # HOSTNAME
 hostnamectl set-hostname thinkpad
 
