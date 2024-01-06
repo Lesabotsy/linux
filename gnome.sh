1b52c07b888ecf5e515f8cc4a5ecf44fec3d9c28
Lesabotsy <moshiro17@gmail.com>
2024-01-06 10:53:27 +0100

    Add files via upload

---
+++ gnome.sh
@@ -0,0 +1,31 @@
+#!/bin/env bash
+
+# RPM FUSION
+dnf install -y \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+dnf install -y \
+    bat \
+    distrobox \
+    fd-find \
+    firefox \
+    fish \
+    fzf \
+    helix \
+    kitty \
+    gnome-shell \
+    nautilus \
+    podman \
+    power-profiles-daemon \
+    shfmt \
+    unrar
+
+dnf install -y \
+    adw-gtk3-theme \
+    breeze-cursor-theme \
+    gnome-tweaks
+
+hostnamectl set-hostname desktop
+systemctl set-default graphical.target
+reboot
