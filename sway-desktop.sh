4958dc48e302491fb8087e55ffe81dd412487f53
RALINORO Hobiniasy <moshiro17@gmail.com>
2024-03-13 15:09:10 +0100

    Add files via upload

---
+++ sway-desktop.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+mkdir /media/usb/
+
+# RPM FUSION
+dnf install -y \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# WIFI
+dnf --setopt=install_weak_deps=False --best install -y \
+    iwl7260-firmware
+
+# BASE
+dnf --setopt=install_weak_deps=False --best install -y \
+    @fonts \
+    dmenu \
+    i3status \
+    sway
+
+dnf --setopt=install_weak_deps=False --best install -y \
+    bat \
+    distrobox \
+    fd-find \
+    feh \
+    ffmpeg \
+    firefox \
+    fish \
+    fzf \
+    gammastep \
+    helix \
+    intel-media-driver \
+    kitty \
+    light \
+    mpv \
+    mupdf \
+    NetworkManager \
+    NetworkManager-wifi \
+    pavucontrol \
+    pipewire \
+    pipewire-pulseaudio \
+    podman \
+    power-profiles-daemon \
+    pulseaudio-utils \
+    ranger \
+    rofi \
+    shfmt \
+    unrar
+    
+# RICE
+dnf --setopt=install_weak_deps=False --best install -y \
+    breeze-cursor-theme papirus-icon-theme \
+    lxappearance
+    
+# HOSTNAME
+hostnamectl set-hostname thinkpad
+
+# STARSHIP
+curl -sS https://starship.rs/install.sh | sh
+
+# DONE
+reboot
