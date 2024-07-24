932d529e2c81ef07084e17c20f821a7ce31119b2
RALINORO Hobiniasy <moshiro17@gmail.com>
2024-07-24 11:41:24 +0200

    Add files via upload

---
+++ kickstart.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+dnf remove -y \
+    vim-minimal
+
+mkdir /media/usb/
+
+# RPM FUSION
+dnf install -y \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# WIFI
+dnf install -y \
+    iwlwifi-mvm-firmware
+
+# BASE
+dnf install -y \
+    sway \
+    kitty \
+    i3status \
+    intel-media-driver \
+    feh \
+    fish \
+    gammastep \
+    light \
+    NetworkManager \
+    NetworkManager-bluetooth \
+    NetworkManager-wifi \
+    pavucontrol \
+    pipewire \
+    pipewire-pulseaudio \
+    pulseaudio-utils \
+    power-profiles-daemon \
+    rofi \
+    seatd
+
+dnf install -y \
+    podman \
+    distrobox \
+    firefox \
+    ffmpeg \
+    fzf \
+    mpv \
+    mupdf \
+    ranger \
+    unrar
+
+# RICE
+dnf install -y \
+    breeze-cursor-theme papirus-icon-theme
+
+# HOSTNAME
+hostnamectl set-hostname thinkpad
+
+# STARSHIP
+curl -sS https://starship.rs/install.sh | sh
